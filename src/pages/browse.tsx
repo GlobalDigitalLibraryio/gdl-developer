6e01160569a851378133a9b7f4650a9b4fef17cf
Kien Vu <kienv91@gmail.com>
2019-03-27 15:15:53 +0100

    set main layout for browse page

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -2,26 +2,29 @@ import React from 'react';
 import { Card, List, ListItem, Typography } from '@material-ui/core';
 import { Link } from 'gatsby';
 import Layout from '../layouts/Layout';
+import { Main } from '../elements';
 
 const BrowsePage = () => (
   <Layout>
-    <Card style={{ padding: 50 }}>
-      <List>
-        <ListItem>
-          <Typography variant="h6">Available</Typography>
-        </ListItem>
-        <ListItem>
-          <Link to="/book-api">
-            <Typography>book-api</Typography>
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/image-api">
-            <Typography>image-api</Typography>
-          </Link>
-        </ListItem>
-      </List>
-    </Card>
+    <Main>
+      <Card style={{ padding: 50 }}>
+        <List>
+          <ListItem component="div">
+            <Typography variant="h6">Available</Typography>
+          </ListItem>
+          <ListItem>
+            <Link to="/book-api">
+              <Typography>book-api</Typography>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/image-api">
+              <Typography>image-api</Typography>
+            </Link>
+          </ListItem>
+        </List>
+      </Card>
+    </Main>
   </Layout>
 );
 
